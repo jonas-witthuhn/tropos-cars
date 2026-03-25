42a9d49c4dc7eca8dafc6fb3ee3ff35c00495456
jonas-witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2026-03-25 10:10:57 +0100

    add ecrad classic cli

--- src/tcars/click.py
+++ src/tcars/click.py
@@ -10,6 +10,9 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 import datetime as dt
+import subprocess
+import jstyleson as json
+import f90nml
 
 import tcars
 import tcars.ecrad
@@ -21,14 +24,52 @@ DEFAULT_CONFIG = fn_config = os.path.join(
         "config/tcars_config.json"
     )
 
+#################################################################################################################
+# T-CARS CLI
+#################################################################################################################
 # initialize commandline interface
 @click.version_option()
 @click.group("tcars")
 def cli_tcars():
     pass
 
-
+#################################################################################################################
+# ecrad cli
+#################################################################################################################
+# init cli
 @click.version_option(message=f"tcars: version {tcars.__version__}, ecrad: version {tcars.ecrad.__version__}")
 @click.group("ecrad")
-def cli_ecrad():
-    pass
+@click.argument("namfile",type=click.Path(dir_okay=False, exists=True), nargs=1)
+@click.argument("infile",type=click.Path(dir_okay=False, exists=True), nargs=1)
+@click.argument("outfile",type=click.Path(dir_okay=False, exists=True), nargs=1)
+@click.option("--config", "-c", type=click.Path(dir_okay=False, exists=True),
+              help="Config file - will merge and override the default config.")
+def cli_ecrad(namfile,infile,outfile,config=None):
+    """Print FILENAME.
+
+    FILENAME is the name of the file to check.
+    """
+    config = tcars.utils.merge_config(config)
+    ecrad_bin = os.path.join(
+        importlib.resources.files("ecrad"),
+        "bin/ecrad"
+    )
+
+    namfile_dict = f90nml.read(namfile)
+
+    pstat = subprocess.run(
+            [ecrad_bin, namfile, infile, outfile],
+            capture_output=True,
+            text=True,
+        )
+    
+    flx = xr.load_dataset(outfile)
+    flx["ECRAD"] = ((), np.int32(0))
+    flx["ECRAD"].attrs = {
+        "version": tcars.ecrad.__version__,
+        "config": json.dumps(namfile_dict),
+        "stdout": pstat.stdout.splitlines(),
+    }
+    
+    flx.to_netcdf(outfile)
+
